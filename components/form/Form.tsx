4f0555b1f8f2fd46cd29d1069b4603d009fdc1d5
Vladyslav Shumkov <71165032+IiIymik@users.noreply.github.com>
2022-05-11 18:12:24 +0300

    added validate for edit user and fixed for add user

--- components/form/Form.tsx
+++ components/form/Form.tsx
@@ -45,15 +45,12 @@ export const AddUserForm = ({ onSubmit }) => {
 
         const form: HTMLFormElement = e.currentTarget;
 
+        if (errors.length) return;
         if (!form.Role.value || form.Role.value === 'Role') {
-          return setErrors([
-            { inputName: 'role', message: 'Select from Role list' },
-          ]);
+          return setErrors([{ message: 'Select from Role list' }]);
         }
         if (form.Department.value === 'Department' || form.Department.value === '') {
-          return setErrors([
-            { inputName: 'department', message: 'Select from Department list' },
-          ]);
+          return setErrors([{ message: 'Select from Department list' }]);
         }
 
         if (
@@ -62,12 +59,11 @@ export const AddUserForm = ({ onSubmit }) => {
         ) {
           return setErrors([
             {
-              inputName: 'department',
               message: 'Check if the fields are filled in correctly',
             },
           ]);
         }
-
+        setErrors([]);
         const newName = `${form.FirstName.value.trim()} ${form.LastName.value.trim()}`;
         onSubmit({
           name: newName,
@@ -115,7 +111,7 @@ export const AddUserForm = ({ onSubmit }) => {
               className="border bg-white border-indigo-50 placeholder-indigo-300 md:text-base w-full"
               onChange={() => setErrors([])}
             >
-              <option value="">Role</option>
+              <option>Role</option>
               {roleList.map((role, i) => (
                 <option key={i} value={role}>
                   {role}
@@ -168,38 +164,34 @@ export const EditUserForm = ({ setIsModuleOpen, user, onSubmit }) => {
   const [departUser, setDepartUser] = useState(
     department ? department : 'Department',
   );
-
-  // const handelChange = ({ target }) => {
-  //   const { value, name } = target;
-  //   switch (name) {
-  //     case 'FirstName':
-  //       setErrors(checkValidateName(value));
-  //       break;
-  //     case 'LastName':
-  //       setErrors(checkValidateSurname(value));
-  //       break;
-  //     case 'Email':
-  //       setErrors(checkValidateEmail(value));
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // };
+  console.log(errors);
+  const handelChange = ({ target }) => {
+    const { value, name } = target;
+    switch (name) {
+      case 'FirstName':
+        setErrors(checkValidateName(value));
+        break;
+      case 'LastName':
+        setErrors(checkValidateSurname(value));
+        break;
+      case 'Email':
+        setErrors(checkValidateEmail(value));
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         const form = e.currentTarget;
-
+        if (errors.length) return;
         if (!form.Role.value || form.Role.value === 'Role') {
-          return setErrors([
-            { inputName: 'role', message: 'Select from Role list' },
-          ]);
+          return setErrors([{ message: 'Select from Role list' }]);
         }
         if (form.Department.value === 'Department' || form.Department.value === '') {
-          return setErrors([
-            { inputName: 'department', message: 'Select from Department list' },
-          ]);
+          return setErrors([{ message: 'Select from Department list' }]);
         }
 
         if (
@@ -208,19 +200,19 @@ export const EditUserForm = ({ setIsModuleOpen, user, onSubmit }) => {
         ) {
           return setErrors([
             {
-              inputName: 'department',
               message: 'Check if the fields are filled in correctly',
             },
           ]);
         }
 
-        // onSubmit({
-        //   name: `${form.FirstName.value} ${form.LastName.value}`,
-        //   email: emailUser,
-        //   role: roleUser,
-        //   id: _id,
-        //   department: departUser,
-        // });
+        setErrors([]);
+        onSubmit({
+          name: `${form.FirstName.value.trim()} ${form.LastName.value.trim()}`,
+          email: emailUser,
+          role: roleUser,
+          id: _id,
+          department: departUser,
+        });
       }}
       className="inline-block w-550 max-w-full"
       onChange={handelChange}
@@ -263,13 +255,17 @@ export const EditUserForm = ({ setIsModuleOpen, user, onSubmit }) => {
           </div>
           <div>
             <select
-              name="role"
+              name="Role"
               id="role"
               className="border bg-white border-indigo-50 placeholder-indigo-300 md:text-base w-full"
               value={roleUser}
-              onChange={({ target }) => setRoleUser(target.value)}
+              onChange={({ target }) => {
+                setRoleUser(target.value);
+                if (target.value !== 'Role' || target.value.length !== 0)
+                  setErrors([]);
+              }}
             >
-              <option value="">Role</option>
+              <option>Role</option>
               {roleList.map((role, i) => (
                 <option key={i} value={role}>
                   {role}
@@ -281,10 +277,15 @@ export const EditUserForm = ({ setIsModuleOpen, user, onSubmit }) => {
             <select
               name="Department"
               id="department"
+              value={departUser}
               className="border bg-white border-indigo-50 placeholder-indigo-300 md:text-base w-full"
-              onChange={({ target }) => setDepartUser(target.value)}
+              onChange={({ target }) => {
+                setDepartUser(target.value);
+                if (target.value !== 'Department' || target.value.length !== 0)
+                  setErrors([]);
+              }}
             >
-              <option value="">{departUser}</option>
+              <option>Department</option>
               {departmentList.map((dept, i) => (
                 <option key={i}>{dept}</option>
               ))}
